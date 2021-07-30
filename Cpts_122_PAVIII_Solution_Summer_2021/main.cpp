dbd6b22ffc1e540074bbb02f91eebe0d653c57d4
Yi Guang Bei <guang-bei.yi@ubisoft.com>
2021-07-31 02:53:59 +0800

    Update pic

--- Cpts_122_PAVIII_Solution_Summer_2021/main.cpp
+++ Cpts_122_PAVIII_Solution_Summer_2021/main.cpp
@@ -4,27 +4,34 @@
 
 int main()
 {
-	//sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+	//sf::RenderWindow window(sf::VideoMode(500, 500), "Cpts 122 PA8");
 
 	Test test;
 	//test.testPush();
 	//test.testPop();
 	//test.testDrawCard();
-	test.testPlayCard();
+	//test.testPlayCard();
 	//test.testShuffle();
-	
-	/*while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
 
-		window.clear();
-		window.display();
-	}*/
+
+	
+	//while (window.isOpen())
+	//{
+	//	sf::Event event;
+	//	while (window.pollEvent(event))
+	//	{
+	//		if (event.type == sf::Event::Closed)
+	//			window.close();
+	//	}
+
+	//	//clear window
+	//	window.clear();
+
+
+
+	//	//display window
+	//	window.display();
+	//}
 
 	return 0;
 }
